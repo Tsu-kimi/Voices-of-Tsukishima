64fb4af48aefb6e8193cd33a174b8044703de1ea
Tsu-kimi <124048152+Tsu-kimi@users.noreply.github.com>
2025-01-04 14:28:34 +0100

    debugged and added jump animation

--- player/player.gd
+++ player/player.gd
@@ -2,53 +2,75 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
-const SPEED = 300
+@export var speed: int = 300
+@export var jump: int = 300
+@export var jump_horizontal: int = 300
 
-enum State { Idle, Run}
-var current_state
+enum State { Idle, Run, Jump } # Added Jump state
+
+var current_state : State
+
+var character_sprite : Sprite2D
+
 
 func _ready():
 	current_state = State.Idle
 
 
-func _physics_process(delta):
+func _physics_process(delta : float):
 	player_falling(delta)
 	player_idle(delta)
 	Player_run(delta)
+	Player_jump(delta) # Added jump function call
 
 	move_and_slide()
 
 	Player_animation()
+	
+	print("State: ", State.keys()[current_state])
 
 
-func player_falling(delta):
+func player_falling(delta : float):
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 
 
-func player_idle(delta):
+func player_idle(delta : float):
 	if is_on_floor():
 		current_state = State.Idle
-		print("State: ", State.keys()[current_state])
 
 
-func Player_run(delta):
-	var direction = Input.get_axis("Move_Left","Move_Right")
+func Player_run(delta : float):
+	var direction = input_movement()
 
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	if direction != 0:
 		current_state = State.Run
-		print("State: ", State.keys()[current_state])
 		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+func Player_jump(delta : float):
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = jump
+		current_state  = State.Jump
+
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = input_movement()
+		velocity.x += direction * jump_horizontal * delta
+
 
 func Player_animation():
 	if current_state == State.Idle:
 		animated_sprite_2d.play("idle")
 	elif current_state == State.Run:
 		animated_sprite_2d.play("run")
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("jump")
 		
-
+func input_movement():
+	var direction :  float =  Input.get_axis("Move_Left", "Move_Right")
+	
+	return direction
